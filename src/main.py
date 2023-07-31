62a991e58f78589089308a6061a023d0862e5362
arseniitelesh <telesh.sen98@gmail.com>
2023-08-01 00:55:10 +0600

    made fixes in utils and main.py

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from utils import get_hh_data, create_database, save_data_to_database
 from config import config
 from class_DBManager import DBManager
-
+import os
 
 
 def main():
@@ -25,15 +25,21 @@ def main():
     create_database('headhunter', params)
     save_data_to_database(data, 'headhunter', params)
 
-    # Прописать свои данные
-    manager = DBManager({'host': 'localhost', 'database': 'headhunter', 'password': '1408', 'port': '5432'})
+    # Прописать свои данные в переменные окружения
+    db_host = os.getenv('host')
+    db_database = os.getenv('database')
+    db_password = os.getenv('password')
+    db_port = os.getenv('port')
+    hh_db_params = {'host': db_host, 'database': db_database, 'password': db_password, 'port': db_port}
+
+    manager = DBManager(hh_db_params)
     manager.connect()
+
     print(manager.get_companies_and_vacancies_count())
     print(manager.get_all_vacancies())
     print(manager.get_avg_salary())
     print(manager.get_vacancies_with_higher_salary())
     print(manager.get_vacancies_with_keyword())
-
 
 
 if __name__ == '__main__':
